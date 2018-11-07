7d1498aab7c361ff17b14d1d619e2a23e4379223
ateliedeaplicativos <44757634+ateliedeaplicativos@users.noreply.github.com>
2018-11-07 01:34:45 -0200

    Update send-mail.php

--- send-mail.php
+++ send-mail.php
@@ -2,9 +2,9 @@
   $nome = $_POST['nome'];
   $email= $_POST['email'];
   $mensagem= $_POST['mensagem'];
-  $to = "curso.irs@hotmail.com";
+  $to = "exemplo@email.com";
   $assunto = "Mensagem do Site";
   $conteudo= "Nome: $nome\nEmail: $email\nMensagem: $mensagem\n\n";
   mail($to,$assunto,$conteudo);
-  header("Location: http://professionalcleaners.com.br/teste");
+  header("Location: http://site.com");
 ?>
